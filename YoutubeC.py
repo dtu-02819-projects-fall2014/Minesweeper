38c61f8f6da5896941cd64f0de558d9eed4d9929
Robert Gutke <robertgutke@hotmail.com>
2014-11-24 17:29:32 +0100

    Checked with flake8

---
+++ YoutubeC.py
@@ -0,0 +1,85 @@
+"""
+This class provides access to the comments of a given Youtube video.
+
+Example:
+    url="https://www.youtube.com/watch?v=5euHa8YMMgk"
+    Y = YoutubeC(url)
+    Y.get_comments()
+    Y.write_comments_to_file()
+
+Created by Robert Gutke / robertgutke@hotmail.com / December 2014
+
+Licensed under the MIT License:
+Copyright (c) 2014, Robert Gutke
+"""
+
+
+import requests
+import urlparse
+import simplejson
+
+
+class YoutubeC:
+    """
+    Provides access to the comments of a given Youtube video.
+    """
+
+    def __init__(self, url):
+        """
+        Args:
+          url (str): The youtube video to be analyzed (as copied from the
+          browser url field)
+        """
+        self.url = url
+        self.video = urlparse.parse_qs(urlparse.urlparse(url).query)["v"][0]
+        self.comments = []
+
+    def write_comments_to_file(self):
+        """
+        Writes the list of comments to a json file
+        """
+        vs = self.video+'.json'
+        f = open(vs, 'w')
+        simplejson.dump(self.comments, f)
+        f.close()
+
+    def get_comments(self):
+        """
+        Takes the url of the youtube video and sends a http request via the
+        youtube API. From the response, it takes the comments and the next
+        link and sends it again to the Youtube API. This is repeated until
+        no more next links are available. The reason for this iterative
+        approach is the fact that Youtube only allows you to get 50 comments
+        at a time.
+
+        Returns:
+          list: list of comments for a Youtube video
+        """
+        next_page = ("http://gdata.youtube.com/feeds/api/videos/"
+                     + self.video
+                     + "/comments?orderby=published&start-index=1"
+                     + "&max-results=50&alt=json")
+
+        while (next_page is not None):
+
+            response = requests.get(next_page)
+
+            if response.status_code == 200:
+
+                # extract the json data from the response
+                data = simplejson.loads(response.text)
+
+                # get the link for the next resultpage
+                if len(data['feed']['link']) > 3:
+                    next_page = data['feed']['link'][3]['href']
+                else:
+                    next_page = None
+
+                # extract the comments
+                for entry in data['feed']['entry']:
+                    self.comments.append(entry['content']['$t'])
+
+            else:
+                return self.comments
+
+        return self.comments
--- sentiment/Emoticon_Sentiment.py
+++ sentiment/Emoticon_Sentiment.py
@@ -1,6 +1,6 @@
 """
-This class provides a binary sentiment analyzer (1 means positive, -1 means negative). 
-It is autmatically created using emoticons from thousands of comments. 
+This class provides a binary sentiment analyzer (1 = positive, -1 = negative).
+It is autmatically created using emoticons from thousands of comments.
 
 Example:
     S=Emoticon_Sentiment("output.txt")
@@ -12,70 +12,64 @@ Created by Robert Gutke / robertgutke@hotmail.com / December 2014
 
 Licensed under the MIT License:
 Copyright (c) 2014, Robert Gutke
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-documentation files (the "Software"), to deal in the Software without restriction, including without
-limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
-Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial portions
-of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
+import HTMLParser
+import re
+import string
+import json
+from random import shuffle
 from pattern.vector import Document, NB
-from pattern.db import csv
-import HTMLParser, re, string,json
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 
 
 class Emoticon_Sentiment:
     """
-    This class provides a binary sentiment analyzer (1 means positive, -1 means negative). 
-    It is autmatically created using emoticons from thousands of comments. 
+    This class provides a binary sentiment analyzer (1 = positive, -1 =
+    negative). It is autmatically created using emoticons from thousands of
+    comments.
     """
 
-    
-    def __init__(self,basefile):
+    def __init__(self, basefile):
         """
         Args:
-          basefile (str): The location of the comments file that should serve as the basis for the classifier.
+          basefile (str): The location of the comments file that should serve
+          as the basis for the classifier.
         """
         self.base = basefile
         self.comments = []
-        self.nb=NB()
-        self.stop=stopwords.words('english')
-        self.positive=[':-)', ':)', ':D' ,':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}', ':^)', ':っ)',':-D', '8-D', '8D', 
-       'x-D', 'xD', 'X-D', 'XD', '=-D', '=D', '=-3', '=3', 'B^D',':-))',':*', ':^*',';-)', ';)', '*-)', '*)', ';-]'
-       ';]', ';D', ';^)']
-        self.negative=['>:[', ':-(' ,':(' , ':-c', ':c' ,':-<',  ':っC', ':<', ':-[', ':[' ,':{',':-||', ':@', '>:(','D:<', 'D:',
-          'D8', 'D;', 'D=', 'DX', 'v.v','>:)', '>;)' ,'>:-)','}:-)' ,'}:)', '3:-)', '3:)',':-###..', ':###..',
-          '>:/', ':-/', ':-.', ':/']
-        
-        
-        
+        self.nb = NB()
+        self.stop = stopwords.words('english')
+        self.positive = [':-)', ':)', ':D', ':o)', ':]', ':3', ':c)', ':>',
+                         '=]', '8)', '=)', ':}', ':^)', ':っ)', ':-D', '8-D',
+                         '8D', 'x-D', 'xD', 'X-D', 'XD', '=-D', '=D', '=-3',
+                         '=3', 'B^D', ':-))', ':*', ':^*', ';-)', ';)', '*-)',
+                         '*)', ';-]', ';]', ';D', ';^)']
+        self.negative = ['>:[', ':-(', ':(', ':-c', ':c', ':-<', ':っC',
+                         ':<', ':-[', ':[', ':{', ':-||', ':@', '>:(', 'D:<',
+                         'D:', 'D8', 'D;', 'D=', 'DX', 'v.v', '>:)', '>;)',
+                         '>:-)', '}:-)', '}:)', '3:-)', '3:)', ':-###..',
+                         ':###..', '>:/', ':-/', ':-.', ':/']
+
     def read_base_comments(self):
         """
-        Reads the file that loads the comments that serve as the base for the classifier.
+        Reads the file that loads the comments that serve as the base for the
+        classifier.
 
         Returns:
           list: list of comments from the text file
         """
         f = open(self.base, 'r')
-        self.comments=json.load(f)
+        self.comments = json.load(f)
         f.close()
         return self.comments
-    
-    
-    
-    def preprocess(self,comment):   
+
+    def preprocess(self, comment):
         """
-        Takes a comment and pre-processes it (all to lowercase, remove links, delete punctuation, remove stopwords and numbers)
-        
+        Takes a comment and pre-processes it (all to lowercase, remove links,
+        delete punctuation, remove stopwords and numbers)
+
         Args:
           comment (str): The text string to be processed
 
@@ -83,78 +77,82 @@ class Emoticon_Sentiment:
           str: the processed text string
         """
         h = HTMLParser.HTMLParser()
-        text=h.unescape(comment.lower())
-        link=r'http:\/\/.*[\r\n]*'
-        p=re.compile(   r"(\b(?:(?:https?|ftp|file)://|www\.|ftp\.)[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|])"   )
-        text=re.sub(p, '', text)
+        text = h.unescape(comment.lower())
+        p = re.compile(r"(\b(?:(?:https?|ftp|file)://|www\.|ftp\.)"
+                       "[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*"
+                       "[-A-Za-z0-9+&@#/%=~_()|])")
+        text = re.sub(p, '', text)
         exclude = set(string.punctuation)
-        tokens=nltk.word_tokenize(text)
-        cleaned=[token for token in tokens if token not in exclude]        
-        text=' '.join([w.encode('ascii',errors='ignore') for w in cleaned if w.lower() not in self.stop and not w.isdigit()])    
+        tokens = word_tokenize(text)
+        cleaned = [token for token in tokens if token not in exclude]
+        text = ' '.join([w.encode('ascii', errors='ignore') for w in cleaned
+                        if w.lower() not in self.stop and not w.isdigit()])
         return text
-    
-    
-    
+
     def parse_comments(self):
         """
-        Parses a list of comments, by searching for emoticons. Once an emoticon is found, it flags it positive or negative. 
+        Parses a list of comments, by searching for emoticons. Once an emoticon
+        is found, it flags it positive or negative.
         With multiple finds, it sums their value; neutrals are thrown out.
 
         Returns:
-          list: 2-dimensional list of parsed comments and their corresponding flags
+          list: 2-dimensional list of parsed comments and their sentiment flags
         """
-        extended_comments = [[self.comments[i],0, False] for i in range(len(self.comments))]
-        
-        #loop through comments and search for emoticons: +1 if positive, -1 if negative, and remove emoticons from text
+        extended_comments = [[self.comments[i], 0, False]
+                             for i in range(len(self.comments))]
+
+        # loop through comments and search for emoticons: +1 if positive, -1
+        # if negative, and remove emoticons from text
         for comment in extended_comments:
-    
-            flagged=False
-                 
-            for n in negative:
-                if comment[0].encode('utf-8').find(n)>-1:
-                    comment[1]=comment[1]-1
-                    comment[0]=comment[0].replace(n, '')
-                    comment[2]=True
-                    
-            for p in positive:
-                if comment[0].encode('utf-8').find(p)>-1:
-                    
-                    comment[1]=comment[1]+1
-                    comment[0]=comment[0].replace(p, '')
-                    comment[2]=True
-        
-        #throw out comments that have no emoticons
-        parsed_comments=[[comment[0],comment[1]] for comment in extended_comments if comment[2]==True]
-        
-        #normalize the values by converting all positives (1,2,3,etc) to 1, and all negatives (-1,-2,-3 etc) to -1
+
+            for n in self.negative:
+                if comment[0].encode('utf-8').find(n) > -1:
+                    comment[1] = comment[1]-1
+                    comment[0] = comment[0].replace(n, '')
+                    comment[2] = True
+
+            for p in self.positive:
+                if comment[0].encode('utf-8').find(p) > -1:
+
+                    comment[1] = comment[1]+1
+                    comment[0] = comment[0].replace(p, '')
+                    comment[2] = True
+
+        # throw out comments that have no emoticons
+        parsed_comments = [[comment[0], comment[1]] for comment
+                           in extended_comments if comment[2]]
+
+        # normalize the values by converting all positives (1,2,3,etc) to 1,
+        # and all negatives (-1,-2,-3 etc) to -1
         for item in parsed_comments:
-            if item[1]>1:
-                item[1]=1
-            elif item[1]<-1:
-                item[1]=-1
-                
-        #throw out neutrals
-        parsed_comments=[[self.preprocess(i[0]),i[1]] for i in parsed_comments if i[1]!=0]
-        
+            if item[1] > 1:
+                item[1] = 1
+            elif item[1] < -1:
+                item[1] = -1
+
+        # throw out neutrals
+        parsed_comments = [[self.preprocess(i[0]), i[1]]
+                           for i in parsed_comments if i[1] != 0]
+
         return parsed_comments
-    
+
     def train_classifier(self, comments):
         """
         takes the flagged comments and trains a Naive Bayes classifier
-        
+
         Args:
           comments (list): The list of flagged comments to be trained
         """
-        shuffle(comments)        
-        
+        shuffle(comments)
+
         for comment in comments:
-            v = Document(comment[0], type=int(comment[1])) 
+            v = Document(comment[0], type=int(comment[1]))
             self.nb.train(v)
-            
+
     def predict(self, text):
         """
         Takes a comment and makes a sentiment prediction
-        
+
         Args:
           text (str): The text string to be analyzed for sentiment
 
