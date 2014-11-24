7b2d5de4e5db4c4dd6d9529f307d3ad8091524d7
Andre <andre.cl@hotmail.com>
2014-11-24 16:23:32 +0100

    working on tests

--- tests/test_Sentiment_analysis.py
+++ tests/test_Sentiment_analysis.py
@@ -8,3 +8,8 @@ def test_if_word_lists_are_there():
     a, b = my_sent.get_word_lists()
     assert len(a) == 3634
     assert len(b) == 2472
+	
+def test_sentiment_with_sample_file():
+    mit, afinn, positivecount, negativecount = my_sent.get_sentiment_values('samples/negtest.txt')
+    assert mit < -1
+	assert afinn < -1
