dfe6d3f4bbbbcd327a80287cd6c979aebbc6a7bf
Jakobok <jakobo123dk@yahoo.dk>
2014-12-04 19:23:40 +0100

    Merged YouTube Video Comparison Website into project

---
+++ app.py
@@ -0,0 +1,75 @@
+import os
+from collections import Counter
+from flask import Flask, render_template, request, redirect, url_for
+from sentiment.Sentiment_analysis import Sentiment_analysis
+from sentiment.YoutubeC import YoutubeC
+import urlparse
+import nltk
+import requests
+import simplejson
+import re
+
+class Video:
+    def __init__(self, url):
+        self.url = url
+        self.id =  urlparse.parse_qs(urlparse.urlparse(self.url).query)["v"][0]
+        self.sentiment_values = self.get_sentiment_values()
+        self.emoticon_sentiment_values = self.get_sentiment_values()
+        self.get_youtube_information()
+
+    def get_sentiment_values(self):
+        youtubeC = YoutubeC(self.url)
+        youtubeC.get_comments()
+        youtubeC.write_comments_to_file()
+        sentiment_values = Sentiment_analysis().get_sentiment_values(self.id + ".json")
+
+        return sentiment_values
+
+    def get_youtube_information(self):
+        response = requests.get("https://gdata.youtube.com/feeds/api/videos/" + self.id + "?v=2&alt=json")
+
+        if response.status_code == 200:
+            data = simplejson.loads(response.text)
+            self.name = data["entry"]["title"]["$t"]
+            self.thumbnail_url = data["entry"]["media$group"]["media$thumbnail"][3]["url"]
+
+app = Flask(__name__, static_url_path = "")
+app.jinja_env.add_extension("jinja2.ext.with_")
+
+def isYoutubeURL(url):
+    youtube_regex = (
+    r'(https?://)?(www\.)?'
+    '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+    '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+
+    youtube_regex_match = re.match(youtube_regex, url)
+    #if youtube_regex_match:
+    #    return youtube_regex_match.group(6)
+
+    return youtube_regex_match
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/comparison")
+def comparison():
+    video_1_url = request.args.get("video_1_url")
+    video_2_url = request.args.get("video_2_url")
+    if isYoutubeURL(video_1_url) and isYoutubeURL(video_2_url):
+
+        video_1 = Video(request.args.get("video_1_url"))
+        video_2 = Video(request.args.get("video_2_url"))
+
+    #return render_template("index.html", error_message = "Error")
+        return render_template(
+            "comparison.html",
+            video_1 = video_1,
+            video_2 = video_2
+        )
+    else:
+        return render_template("index.html", error_message = "Error")
+
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host = "0.0.0.0", port = port)
